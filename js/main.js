f729d5d5e1391ec423190d6989ecd15ff2d4b4ae
Qaena <cam.opperman@gmail.com>
2024-05-20 11:42:08 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -48,6 +48,13 @@ function permission () {
                   // Extract device's orientation data (e.g., pitch)
                   let pitch = Math.atan2(acceleration.x, Math.sqrt(acceleration.y * acceleration.y + acceleration.z * acceleration.z));
                   document.querySelector(".orientationTest").innerHTML = pitch;
+
+                
+                  if (Math.abs(pitch) < 0.75) {
+                    waitingForTipUp = true;
+                  } else if (Math.abs(pitch) > 1.1 && waitingForTipUp) {
+                    triggerNextWord();
+                  }
               })
           }
       })
