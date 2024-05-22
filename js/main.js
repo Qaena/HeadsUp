783b1d5712ae28e5ef077acabcc968450ad6ebfa
Cameron <cam.opperman@gmail.com>
2024-05-21 19:58:40 -0700

    Take out motion test.

--- js/main.js
+++ js/main.js
@@ -49,7 +49,6 @@ function permission () {
                 let acceleration = event.accelerationIncludingGravity;
                 // Extract device's orientation data (e.g., pitch)
                 let pitch = Math.atan2(acceleration.x, Math.sqrt(acceleration.y * acceleration.y + acceleration.z * acceleration.z));
-                document.querySelector(".orientationTest").innerHTML = pitch;
 
                 if (Math.abs(pitch) < 0.75) {
                   waitingForTipUp = true;
